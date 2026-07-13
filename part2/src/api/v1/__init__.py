b3ea3127c166c2e80835a6734f5c48e5b166c971
Sara <sarakhalid.b28@gmail.com>
2026-07-13 15:50:35 +0300

    Initialize API version 1 with user and amenities namespaces

---
+++ part2/src/api/v1/__init__.py
@@ -0,0 +1,18 @@
+# src/api/v1/__init__.py
+from flask import Blueprint
+from flask_restx import Api
+from src.api.v1.users import api as users_ns
+from src.api.v1.amenities import api as amenities_ns  # 1. استدعاء ملف المرافق
+
+v1_blueprint = Blueprint('api_v1', __name__, url_prefix='/api/v1')
+
+api = Api(
+    v1_blueprint,
+    title='HBnB API',
+    version='1.0',
+    description='HBnB Application API',
+    doc='/docs'
+)
+
+api.add_namespace(users_ns, path='/users')
+api.add_namespace(amenities_ns, path='/amenities')  # 2. تسجيل مسار المرافق
