674178d5bb608837daad2a7a19f247331df80158
dana <trivian1966@gmail.com>
2026-07-16 22:31:14 +0200

    test and review

--- part2/app/services/__init__.py
+++ part2/app/services/__init__.py
@@ -1,3 +1,6 @@
 from app.services.facade import HBnBFacade
 
+# Create a singleton instance of the facade
 facade = HBnBFacade()
+
+__all__ = ['facade']
--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,86 +1,125 @@
 from app.persistence.repository import InMemoryRepository
+from app.models.user import User
+from app.models.place import Place
+from app.models.review import Review
+from app.models.amenity import Amenity
 
 
 class HBnBFacade:
+    """
+    Facade class that manages communication between the Presentation,
+    Business Logic, and Persistence layers.
+    """
+    
     def __init__(self):
         self.user_repo = InMemoryRepository()
         self.place_repo = InMemoryRepository()
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    # ---------- User Methods ----------
-
-    def create_user(self, user):
+    # ========== USER OPERATIONS ==========
+    
+    def create_user(self, user_data):
+        """Create a new user"""
+        user = User(**user_data)
         self.user_repo.add(user)
         return user
 
     def get_user(self, user_id):
+        """Retrieve a user by ID"""
         return self.user_repo.get(user_id)
 
     def get_all_users(self):
+        """Retrieve all users"""
         return self.user_repo.get_all()
 
+    def update_user(self, user_id, user_data):
+        """Update a user by ID"""
+        user = self.user_repo.get(user_id)
+        if user:
+            user.update(user_data)
+            return user
+        return None
+
     def get_user_by_email(self, email):
+        """Retrieve a user by email"""
         return self.user_repo.get_by_attribute('email', email)
 
-    def update_user(self, user_id, data):
-        self.user_repo.update(user_id, data)
-        return self.user_repo.get(user_id)
-
-    # ---------- Amenity Methods ----------
-
-    def create_amenity(self, amenity):
+    # ========== AMENITY OPERATIONS ==========
+    
+    def create_amenity(self, amenity_data):
+        """Create a new amenity"""
+        amenity = Amenity(**amenity_data)
         self.amenity_repo.add(amenity)
         return amenity
 
     def get_amenity(self, amenity_id):
+        """Retrieve an amenity by ID"""
         return self.amenity_repo.get(amenity_id)
 
     def get_all_amenities(self):
+        """Retrieve all amenities"""
         return self.amenity_repo.get_all()
 
-    def update_amenity(self, amenity_id, data):
-        self.amenity_repo.update(amenity_id, data)
-        return self.amenity_repo.get(amenity_id)
+    def update_amenity(self, amenity_id, amenity_data):
+        """Update an amenity by ID"""
+        amenity = self.amenity_repo.get(amenity_id)
+        if amenity:
+            amenity.update(amenity_data)
+            return amenity
+        return None
 
-    # ---------- Place Methods ----------
-
-    def create_place(self, place):
+    # ========== PLACE OPERATIONS ==========
+    
+    def create_place(self, place_data):
+        """Create a new place"""
+        place = Place(**place_data)
         self.place_repo.add(place)
         return place
 
     def get_place(self, place_id):
+        """Retrieve a place by ID"""
         return self.place_repo.get(place_id)
 
     def get_all_places(self):
+        """Retrieve all places"""
         return self.place_repo.get_all()
 
-    def update_place(self, place_id, data):
-        self.place_repo.update(place_id, data)
-        return self.place_repo.get(place_id)
+    def update_place(self, place_id, place_data):
+        """Update a place by ID"""
+        place = self.place_repo.get(place_id)
+        if place:
+            place.update(place_data)
+            return place
+        return None
 
-    # ---------- Review Methods ----------
-
-    def create_review(self, review):
+    # ========== REVIEW OPERATIONS ==========
+    
+    def create_review(self, review_data):
+        """Create a new review"""
+        review = Review(**review_data)
         self.review_repo.add(review)
         return review
 
     def get_review(self, review_id):
+        """Retrieve a review by ID"""
         return self.review_repo.get(review_id)
 
     def get_all_reviews(self):
+        """Retrieve all reviews"""
         return self.review_repo.get_all()
 
-    def update_review(self, review_id, data):
-        self.review_repo.update(review_id, data)
-        return self.review_repo.get(review_id)
-
-    def delete_review(self, review_id):
-        self.review_repo.delete(review_id)
+    def update_review(self, review_id, review_data):
+        """Update a review by ID"""
+        review = self.review_repo.get(review_id)
+        if review:
+            review.update(review_data)
+            return review
+        return None
 
     def get_reviews_by_place(self, place_id):
-        reviews = self.review_repo.get_all()
-        return [
-            review for review in reviews
-            if review.place.id == place_id
-        ]
+        """Retrieve all reviews for a specific place"""
+        place = self.place_repo.get(place_id)
+        if place:
+            return place.reviews
+        return []
