08801b1f9a5fc2457f9cc6f1e51e54d7b78f024f
bothainakb <bothainakb@gmail.com>
2026-07-26 18:19:54 +0000

    __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restx import Api
 
-from app.config import Config
+from app.config import config
 
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
@@ -9,10 +9,10 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 
 
-def create_app(config_class=Config):
+def create_app(config_name='default'):
     app = Flask(__name__)
 
-    app.config.from_object(config_class)
+    app.config.from_object(config[config_name])
 
     api = Api(
         app,
