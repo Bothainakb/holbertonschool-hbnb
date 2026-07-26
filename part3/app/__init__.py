7b0151bbb5df56db143ac0e350d77691d7d0b9d6
bothainakb <bothainakb@gmail.com>
2026-07-26 18:12:01 +0000

    __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask_restx import Api
 
+from app.config import Config
+
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 
-def create_app():
+
+def create_app(config_class=Config):
     app = Flask(__name__)
+
+    app.config.from_object(config_class)
 
     api = Api(
         app,
@@ -17,7 +22,6 @@ def create_app():
         doc='/api/v1/'
     )
 
-    # Register API namespaces
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(users_ns, path='/api/v1/users')
