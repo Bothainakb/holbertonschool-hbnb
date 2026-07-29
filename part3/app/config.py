6cd9299f4134ce2c9f361ae3247374154ced24be
bothainakb <bothainakb@gmail.com>
2026-07-29 03:29:42 +0000

    config.py

--- part3/app/config.py
+++ part3/app/config.py
@@ -2,13 +2,14 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
-    JWT_SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
+    SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
     DEBUG = False
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///development.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 config = {
