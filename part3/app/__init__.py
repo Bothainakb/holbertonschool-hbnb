c3efdf1cd3a6917e7649d775a3b7009b7f841c83
bothainakb <bothainakb@gmail.com>
2026-07-29 03:32:05 +0000

    __init__.py

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_restx import Api
 from flask_jwt_extended import JWTManager
+
 from app.config import config
 
 from app.api.v1.places import api as places_ns
@@ -9,13 +11,18 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.auth import api as auth_ns
 
+# Initialize extensions
+db = SQLAlchemy()
 jwt = JWTManager()
+
 
 def create_app(config_name='default'):
     app = Flask(__name__)
 
     app.config.from_object(config[config_name])
-    
+
+    # Initialize extensions
+    db.init_app(app)
     jwt.init_app(app)
 
     api = Api(
@@ -26,6 +33,7 @@ def create_app(config_name='default'):
         doc='/api/v1/'
     )
 
+    # Register namespaces
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(users_ns, path='/api/v1/users')
