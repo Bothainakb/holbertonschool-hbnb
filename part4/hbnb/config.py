2ca03ae0d6c48e38b3ea8407ec5288e6a6c14e9b
bothainakb <bothainakb@gmail.com>
2026-08-19 12:15:07 +0000

    config.py

--- part4/hbnb/config.py
+++ part4/hbnb/config.py
@@ -1,11 +1,19 @@
 import os
 
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URL',
+        'sqlite:///hbnb.db'
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
+
 
 config = {
     'development': DevelopmentConfig,
